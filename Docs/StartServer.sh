23de1fdaf2f7441b73a5adb85fb9d967ca823de2
eejai42 <start@anabstractlevel.com>
2017-11-21 12:41:49 -0600

    Removed "extra" default files. - First commit will be noisier, but the initial repo will be simpler - better.

--- Docs/StartServer.sh
+++
@@ -1,3 +0,0 @@
-
-npm install http-server
-./node_modules/.bin/http-server -o
--- SqlServer/UpdateSqlSchema.sql
+++
@@ -1,9 +0,0 @@
-
-DECLARE @ObjectName NVARCHAR(100)
-
-    
-
-SELECT *
-FROM (SELECT 'true' AS Success) AS Results
-FOR XML AUTO
-                   
