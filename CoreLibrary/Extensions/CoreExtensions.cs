6191a3751da9e3f99c7a081aeb9548ddb310d5ae
eejai42 <start@anabstractlevel.com>
2017-11-26 13:29:16 -0600

    Updated Nuget Packages in WebApp.

---
+++ CoreLibrary/Extensions/CoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Extensions
+{
+    public static class CoreExtensions
+    {
+        public static String SafeToString(this object obj)
+        {
+            if (ReferenceEquals(obj, null)) return String.Empty;
+            else return obj.ToString();
+        }
+    }
+}
